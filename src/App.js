b2398e8d49d9f7677996f1ec979c70a09c62f604
SANGAMESH KALBEMULLGE <sanguvk123@gmail.com>
2021-03-25 19:48:37 +0530

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Navbar from './components/Navbar';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact />
+      </Switch>
+    </Router>
   );
 }
 export default App;
