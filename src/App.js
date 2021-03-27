0f873a1c651ff943366fb7d1e8aa2c932e185dad
SANGAMESH KALBEMULLGE <sanguvk123@gmail.com>
2021-03-27 14:51:48 +0530

    frame completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import './App.css';
 import Home from './components/pages/Home';
+import Details from './components/pages/Details';
+import Projects from './components/pages/Projects';
+import About from './components/pages/About';
+
 
 function App() {
   return (
+    <>
     <Router>
       <Navbar />
       <Switch>
         <Route path='/' exact component = { Home } />
+        <Route path='/details' exact component = { Details } />
+        <Route path='/projects' exact component = { Projects } />
+        <Route path='/about' exact component = { About } />
       </Switch>
+      <Footer />
     </Router>
+    </>
   );
 }
 export default App;
---
+++ src/components/pages/Details.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Details() {
+    return <h1 className="Ddetails">Details</h1>
+}
